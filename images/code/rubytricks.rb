16bfecc9cbcd39795f62472363a844abadf9b98c
Amber Tunnell <amber.tunnell@gmail.com>
2014-06-16 02:11:54 -0400

    Site updated at 2014-06-16 06:11:54 UTC

--- images/code/rubytricks.rb
+++ images/code/rubytricks.rb
@@ -19,4 +19,28 @@ array.freeze
 array.collect! {|num| num * 27}
   #=> "RuntimeError: can't modify frozen Array"
 array.frozen? 
-  #=> true  
+  #=> true 
+
+#Convert a number to a different base
+2.to_s(2) #=> "10"
+
+5678437.to_s(2) #=> "10101101010010101100101"
+5678437.to_s(23) #=> "k6g6d"
+
+#Convert array to hash
+animals = ["cat","meow","snake","hiss"]
+Hash[*animals] #=> {"cat"=>"meow", "snake"=>"hiss"} 
+
+#Using splat in arguments
+
+#Takes an optional number of arguments
+def splat_is_awesome(*a)
+  a
+end
+splat_is_awesome("and","it","looks","cool","too")  
+ # => ["and", "it", "looks", "cool", "too"] 
+
+def splat_power(*a,b)
+  a,b
+end
+splat_power("powerful","stuff","here")
